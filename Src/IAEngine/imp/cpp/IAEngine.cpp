bf958cc2a946f63a8af348e18d4853832bfbe850
Isuru Samarathunga <isuru.00.aravinda@gmail.com>
2025-07-04 20:14:29 +0530

    Initial

---
+++ Src/IAEngine/imp/cpp/IAEngine.cpp
@@ -0,0 +1,6 @@
+#include <IAEngine/IAEngine.hpp>
+
+namespace ia::iae
+{
+    
+}
---
+++ Src/IAEngine/inc/IAEngine/IAEngine.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iacore/base.hpp>
+#include <iacore/string.hpp>
+
+namespace ia::iae
+{
+    
+}
